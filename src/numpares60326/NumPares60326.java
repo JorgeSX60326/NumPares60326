21cb8d4ac35fdc0d70825906f9645b246cd5ac31
Jun <Jun@deivydgeorge>
2021-02-24 20:44:10 -0600

    Commit completo

---
+++ src/numpares60326/NumPares60326.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares60326;
+
+/**
+ *
+ * @author Jun
+ */
+public class NumPares60326 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num = 0;
+        while (num < 48) {
+             if (num%2 == 0) {
+             System.out.println("num = " + num);
+             
+             }
+             num = num + 1;
+    }
+    }
+    
+}
